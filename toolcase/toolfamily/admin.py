ae3c21fe58fb775a34954851e5f806e18ca34607
Yuehchi Chi <37394024+YuehChi@users.noreply.github.com>
2022-03-24 17:42:21 +0800

    Revert "Revert "Revert "Merge branch '測試----資料庫建置' of https://github.com/YuehChi/ToolBox into 測試----資料庫建置"""
    This reverts commit e3163768b6130b6995a0e99bb2c9e30c632d07db.

--- toolcase/toolfamily/admin.py
+++ toolcase/toolfamily/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-# multi location/language support
-# from django.conf.locale.es import formats as es_formats
-# from django.conf.locale.zh import formats as zh_formats
-
-# chamge date-time format
-# es_formats.DATETIME_FORMAT = "d M Y H:i:s"
-# cn_formats.DATETIME_FORMAT = "d M Y H:i:s"
-
-
 # Register your models here.
 
 
@@ -23,7 +14,6 @@ class UserDetailAdmin(admin.ModelAdmin):
         'rate',
         'created_datetime'
     )
-    readonly_fields = ('created_datetime','user_id')
 admin.site.register(UserDetail, UserDetailAdmin)
 
 
@@ -31,15 +21,15 @@ admin.site.register(UserDetail, UserDetailAdmin)
 class CaseAdmin(admin.ModelAdmin):
     save_as = True
     list_display = (
+        'case_id',
         'title',
         'publisher',
         'reward',
         'location',
         'ended_datetime',
-        'case_status',
+        'status',
         'shown_public'
     )
-    readonly_fields = ('created_datetime', 'last_change', 'case_id')
 admin.site.register(Case, CaseAdmin)
 
 
@@ -48,11 +38,9 @@ class CaseWillingnessAdmin(admin.ModelAdmin):
     save_as = True
     list_display = (
         'casewillingness_id',
-        'willing_user',
-        'apply_case',
+        'case',
         'created_datetime'
     )
-    readonly_fields = ('created_datetime', 'casewillingness_id')
 admin.site.register(CaseWillingness, CaseWillingnessAdmin)
 
 
@@ -63,13 +51,12 @@ class CommissionRecordAdmin(admin.ModelAdmin):
         'commissionrecord_id',
         'case',
         'commissioned_user',
-        'user_status',
-        'rate_publisher_to_worker',
-        'rate_worker_to_publisher',
+        'status',
+        'rate_toolman',
+        'rate_case_publisher',
         'created_datetime',
         'finish_datetime'
     )
-    readonly_fields = ('created_datetime', 'commissionrecord_id')
 admin.site.register(CommissionRecord)
 
 
@@ -82,24 +69,15 @@ class ReportAdmin(admin.ModelAdmin):
         'reporter',
         'reported_case',
         'reported_user',
-        'is_treated',
-        'is_valid'
+        'status',
+        'confirmed'
     )
-    readonly_fields = ('created_datetime', 'report_id')
 admin.site.register(Report, ReportAdmin)
 
 
 
-class StatusAdmin(admin.ModelAdmin):
-    save_as = True
-    list_display = (
-        'status_id',
-        'status_name'
-    )
-admin.site.register(Status, StatusAdmin)
-
-
 # 這邊是不做處理、直接用預設介面的後台
+admin.site.register(Status)
 admin.site.register(CasePhoto)
 admin.site.register(Type)
 admin.site.register(Case_Type)
