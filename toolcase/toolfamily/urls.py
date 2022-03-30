9220481118272aed16442131364e6cd11ce97ee1
Yuehchi Chi <37394024+YuehChi@users.noreply.github.com>
2022-03-31 00:02:27 +0800

    Post form finished

--- toolcase/toolfamily/urls.py
+++ toolcase/toolfamily/urls.py
@@ -8,13 +8,19 @@ urlpatterns = [
 
 
 
-## Case urls
+
+#####################################
+#           CASE URLS               #
+#####################################
 urlpatterns += [
     path('case/',views.index, name='index'),
     path('case/new/',views.case_new, name='case-new'),
     path('case/profile/<int:case_id>/', views.case_profile, name='case-profile' ),
     path('case/profile/<int:case_id>/edit/',views.case_profile_edit, name='case-profile-edit'),
     path('case/search/', views.case_search,name='case-search'),
+    #### use temp url
+    path('case/new_temp/',views.case_new_temp, name='new_temp'),
+    path('case/case_all_temp/',views.case_all_temp, name='case_all_temp'),
 ]
 
 
--- toolcase/toolfamily/views.py
+++ toolcase/toolfamily/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required, permission_required
-from .models import Status,Case,CasePhoto,Type,Case_Type,Field,Case_Field
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -20,30 +20,74 @@ def index(request):
 
 
 
-# #### CASE ######
-# #======新增CASE============
+#####################################
+#           CASE MODULE             #
+#####################################
+
+#-------------新增CASE---------------
+@login_required
 def case_new(request):
 
     ### 登入權限判斷
-    if not request.user.is_authenticated:
-       return render(request,'index.html')
+    # if not request.user.is_authenticated:
+    #    return render(request,'index.html')
     
-    else:
+    # else:
 
-        if request.method == "POST":
+    if request.method == "POST" :
+
+        #委託人-外部鍵
+        publisher = UserDetail.objects.get(user_id = "4")
+
+        #狀態-外部鍵
+        case_status = Status.objects.get(status_id = "1")
+
+        # 委託資訊
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        reward = request.POST.get('reward')
+        location = request.POST.get('location')
+        constraint = request.POST.get('constraint')
+
+        # 時間
+        started_datetime = request.POST.get('started_datetime')  # 任務開始時間（預設為空）
+        ended_datetime = request.POST.get('ended_datetime')
+
+        case = Case(title = title ,publisher = publisher, description = description , reward = reward, location = location , 
+        constraint = constraint, started_datetime = started_datetime , ended_datetime = ended_datetime,case_status =case_status)
+        case.save()
 
 
+        # 外部鍵 case (抓最新一筆關聯)
+        case = Case.objects.first()
+
+        # 照片
             
-            return render(request,'case/new.html',context=context)
+        image = request.FILES.get('photo_image')
+        casephoto = CasePhoto(image = image , case = case)
+        casephoto.save()
 
-        context ={
+        # 類型
+        type_temp = request.POST.getlist('case_type')
+        for i in range(len(type_temp)):
+            type = Type.objects.get(type_id = type_temp[i])
+            case_type = Case_Type(case_type = type, case = case )
+            case_type.save()
 
-        }
+        # 領域 
+        field_temp = request.POST.getlist('case_field')
+        for i in range(len(field_temp)):
+            field = Field.objects.get(field_id = field_temp[i])
+            case_field = Case_Field(case_field = field ,case = case)
+            case_field.save()
 
+        return render(request,'case/case_all_temp.html') 
 
-        return render(request,'case/new.html',context=context)
+   
 
-# #======詳細CASE資訊============
+    return render(request,'case/new.html')
+
+#-------------詳細CASE資訊-------------
 def case_profile(request):
 
     context ={
@@ -53,7 +97,7 @@ def case_profile(request):
     return render(request,'case/profile.html',context=context)
 
 
-# #======編輯CASE資訊============
+#-------------編輯CASE資訊-------------
 def case_profile_edit(request):
 
     context ={
@@ -62,12 +106,99 @@ def case_profile_edit(request):
 
     return render(request,'case/profile_edit.html',context=context)
 
-# #======CASE資訊搜尋============
+# -------------CASE資訊搜尋-------------
 @login_required
 def case_search(request):
 
-    list_case = Case.objects.all()
+    list_case = Case.objects.filter(shown_public=True)
+    case_fields = Case_Field.objects.all()
+    case_types = Case_Type.objects.all()
+    case_photo = CasePhoto.objects.all()
+
 
     #list_case = Case.objects.select_related('case_status')
-    return render(request,'case/search.html',locals())
+    return render(request,'case/search.html',locals()) #之後要改
+
+
+
+#---------------temp--------------
+
+#-------------新增CASE---------------
+@login_required
+def case_new_temp(request):
+
+    ### 登入權限判斷
+    # if not request.user.is_authenticated:
+    #    return render(request,'index.html')
+    
+    # else:
+
+    if request.method == "POST" :
+
+        #委託人-外部鍵
+        publisher = UserDetail.objects.get(user_id = "4")
+
+        #狀態-外部鍵
+        case_status = Status.objects.get(status_id = "1")
+
+        # 委託資訊
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        reward = request.POST.get('reward')
+        location = request.POST.get('location')
+        constraint = request.POST.get('constraint')
+
+        # 時間
+        started_datetime = request.POST.get('started_datetime')  # 任務開始時間（預設為空）
+        ended_datetime = request.POST.get('ended_datetime')
+
+        case = Case(title = title ,publisher = publisher, description = description , reward = reward, location = location , 
+        constraint = constraint, started_datetime = started_datetime , ended_datetime = ended_datetime,case_status =case_status)
+        case.save()
+
+
+        # 外部鍵 case (抓最新一筆關聯)
+        case = Case.objects.first()
+
+        # 照片
+            
+        image = request.FILES.get('photo_image')
+        casephoto = CasePhoto(image = image , case = case)
+        casephoto.save()
+
+        # 類型
+        type_temp = request.POST.getlist('case_type')
+        for i in range(len(type_temp)):
+            type = Type.objects.get(type_id = type_temp[i])
+            case_type = Case_Type(case_type = type, case = case )
+            case_type.save()
+
+        # 領域 
+        field_temp = request.POST.getlist('case_field')
+        for i in range(len(field_temp)):
+            field = Field.objects.get(field_id = field_temp[i])
+            case_field = Case_Field(case_field = field ,case = case)
+            case_field.save()
+
+        return render(request,'case/case_all_temp.html') 
+
+   
+
+    return render(request,'case/new_temp.html')
+
+
+
+
+
+@login_required
+def case_all_temp(request):
+
+    list_case = Case.objects.filter(shown_public=True)
+    case_fields = Case_Field.objects.all()
+    case_types = Case_Type.objects.all()
+    case_photo = CasePhoto.objects.all()
+
+
+    #list_case = Case.objects.select_related('case_status')
+    return render(request,'case/case_all_temp.html',locals()) #之後要改
 
