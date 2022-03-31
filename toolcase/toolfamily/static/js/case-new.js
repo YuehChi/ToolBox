fe1cd2a03c9b1d0a25d9db04903448c4d28ab391
Yucha0608 <thescream880608@gmail.com>
2022-03-31 16:15:05 +0800

    case-new 0331 16:15

---
+++ toolcase/toolfamily/static/js/case-new.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#summernote').summernote({
+      placeholder: 'Hello Bootstrap 4',
+      tabsize: 2,
+      height: 100
+  });
+
+  $('.option').hover(function(){
+    console.log("1")
+    
+    
+  })
+});
