fd480469beaa0f4e41f17e0defdeea0121f677ef
Yucha0608 <thescream880608@gmail.com>
2022-04-01 12:48:46 +0800

    case-new update

--- toolcase/toolfamily/static/js/case-new.js
+++ toolcase/toolfamily/static/js/case-new.js
@@ -14,6 +14,28 @@ $(document).ready(function() {
 
   });
 
+  $('#radio-check-enddate').click(function(){
+    var source = $(this).attr('src').split("/");
+    if(source[source.length -1] == 'check-circle-outline.png'){
+      $("#input-enddate").attr('disabled', true);
+      $(this).attr('src', '/static/images/check-circle.png')
+    }
+    else{
+      $("#input-enddate").attr('disabled', false);
+      $(this).attr('src', '/static/images/check-circle-outline.png')
+    }
+  
+  });
+
+  $('#input-required-NPER').on('input', function() {
+    if($(this).val()){
+
+    }
+  });
+
+
+
+
   // var selector = $(location).prop("href").split("/").slice(0,-3).join("/");
   // var selector = selector + "/static/plugin/multiselect-02/colorlib-selector.html"
   // $('.multi-selector').load(selector); 
