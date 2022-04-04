18544a82cadaff9ca8f7bfe71406e60bd1a39cfa
Yuehchi Chi <37394024+YuehChi@users.noreply.github.com>
2022-04-04 20:22:43 +0800

    Delete temp

--- toolcase/toolfamily/urls.py
+++ toolcase/toolfamily/urls.py
@@ -18,9 +18,6 @@ urlpatterns += [
     path('case/profile/<int:case_id>/', views.case_profile, name='case-profile' ),
     path('case/profile/<int:case_id>/edit/',views.case_profile_edit, name='case-profile-edit'),
     path('case/search/', views.case_search,name='case-search'),
-    #### use temp url
-    path('case/new_temp/',views.case_new_temp, name='new_temp'),
-    path('case/case_all_temp/',views.case_all_temp, name='case_all_temp'),
 ]
 
 
--- toolcase/toolfamily/views.py
+++ toolcase/toolfamily/views.py
@@ -70,10 +70,6 @@ def case_new(request):
 
         # 照片
             
-        # image = request.FILES.get('photo_image')
-        # casephoto = CasePhoto(image = image , case = case)
-        # casephoto.save()
-
         image = request.FILES.getlist('photo_image')
         for f in image:
             file = CasePhoto(image=f, case = case)
@@ -216,103 +212,4 @@ def case_search(request):
     return render(request,'case/search.html',locals()) #之後要改
 
 
-
-
-
-#---------------temp--------------
-#======================================================================================
-#-------------新增CASE---------------
-@login_required
-def case_new_temp(request):
-
-    ### 登入權限判斷
-    # if not request.user.is_authenticated:
-    #    return render(request,'index.html')
-    
-    # else:
-
-    if request.method == "POST" :
-
-        #委託人-外部鍵
-        publisher = UserDetail.objects.get(user_id = "4")
-
-        #狀態-外部鍵
-        case_status = Status.objects.get(status_id = "1")
-
-        # 委託資訊
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        reward = request.POST.get('reward')
-        location = request.POST.get('location')
-        constraint = request.POST.get('constraint')
-
-        # 時間
-        started_datetime = request.POST.get('started_datetime')  # 任務開始時間（預設為空）
-        ended_datetime = request.POST.get('ended_datetime')
-
-        # 新增 人數 、工作方式
-
-        num = request.POST.get('num')
-        work = request.POST.get('work')
-
-        case = Case(title = title ,publisher = publisher, description = description , reward = reward, location = location , 
-        constraint = constraint, started_datetime = started_datetime , ended_datetime = ended_datetime,case_status =case_status,
-        num = num , work = work)
-        case.save()
-
-
-
-        # 外部鍵 case (抓最新一筆關聯)
-        case = Case.objects.first()
-
-        # 照片
-            
-        image = request.FILES.getlist('photo_image')
-        for f in image:
-            file = CasePhoto(image=f, case = case)
-            file.save()
-
-        # 類型
-        type_temp = request.POST.getlist('case_type')
-        for i in range(len(type_temp)):
-            type = Type.objects.get(type_id = type_temp[i])
-            case_type = Case_Type(case_type = type, case = case )
-            case_type.save()
-
-        # 領域 
-        field_temp = request.POST.getlist('case_field')
-        for i in range(len(field_temp)):
-            field = Field.objects.get(field_id = field_temp[i])
-            case_field = Case_Field(case_field = field ,case = case)
-            case_field.save()
-
-        
-        pk_key = case.case_id
-
-        list_case = Case.objects.filter(Q(case_id=pk_key) & Q(shown_public=True))  
-        case_fields = Case_Field.objects.filter(case = pk_key)  
-        case_types = Case_Type.objects.filter(case = pk_key)   
-        case_photo = CasePhoto.objects.filter(case = pk_key)   
-
-        return redirect('case-profile',case_id = pk_key)
-      
-
-
-    return render(request,'case/new_temp.html',locals())
-
-
-
-
-
-@login_required
-def case_all_temp(request):
-
-    list_case = Case.objects.filter(shown_public=True)
-    case_fields = Case_Field.objects.all()
-    case_types = Case_Type.objects.all()
-    case_photo = CasePhoto.objects.all()
-
-
-    #list_case = Case.objects.select_related('case_status')
-    return render(request,'case/case_all_temp.html',locals()) #之後要改
 
