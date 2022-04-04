56a44d74504e2d82bd7e92799f9ce636e4dcf659
meow <ivy8792@gmail.com>
2022-04-05 01:44:48 +0800

    Fix the form redirect bug

--- toolcase/toolfamily/urls.py
+++ toolcase/toolfamily/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('logout/', views.logout),
-    path('register/', views.register),
-    path('activate/<str:token>', views.active),
-    path('forget/', views.forget),
-    path('reset/<str:token>', views.reset),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('activate/<str:token>', views.active, name='active'),
+    path('forget/', views.forget, name='forget'),
+    path('reset/<str:token>', views.reset, name='reset'),
     path('home/', views.index, name='index'),
 ]
 
