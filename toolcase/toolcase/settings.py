a6146a256bd7199223d2165e2fef5df772641b29
r09725039 <48677950+Ting0-0@users.noreply.github.com>
2022-05-04 16:37:04 +0800

    debug settings.py & DB
    - add: replace <image> with <div style="background-image"..."> on navbar user icon

--- toolcase/toolcase/settings.py
+++ toolcase/toolcase/settings.py
@@ -27,7 +27,12 @@ DEBUG = True
 
 #ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['https://ntutoolbox.herokuapp.com/']
+# ALLOWED_HOSTS = ['https://ntutoolbox.herokuapp.com/']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://ntutoolbox.herokuapp.com/'
+]
 
 
 # Application definition
@@ -152,7 +157,7 @@ else:
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # #設定上傳檔案存放的目錄
 # MEDIA_URL = '/media/'
@@ -178,5 +183,5 @@ EMAIL_HOST_PASSWORD = 'ixbdessacfgleknl'
 # USE_L10N = True  # 允許依地區採用不同時間日期格式(會覆蓋其他設定)
 DATETIME_FORMAT = 'Y/m/d H:i:s'  # 指定時間日期格式
 SHORT_DATETIME_FORMAT = 'm/d H:i'  # 指定縮寫的時間日期格式
-import django_on_heroku
-django_on_heroku.settings(locals())
+# import django_on_heroku
+# django_on_heroku.settings(locals())
