b76e0872f98405b238a545f59cbec5f8bd186787
hsinyu521 <gudetama2734@gmail.com>
2022-05-07 16:17:35 +0800

    circle image v2

--- toolcase/toolfamily/static/js/user.js
+++ toolcase/toolfamily/static/js/user.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
         if(input.files && input.files[0]){
             const reader = new FileReader();
             reader.onload = function (e) {
+                // var preimg = document.querySelector('#preview_img');
+                // preimg.style.background = "url("+e.target.result+")";
                 $("#preview_img").attr('src', e.target.result);
             }
             reader.readAsDataURL(input.files[0]);
